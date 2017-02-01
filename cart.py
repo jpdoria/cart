c588218a13ed48cbb7336e611575e5b59590cb62
jpdoria <jp@lazyadm.in>
2017-02-01 12:24:43 +0800

    Minor edit

--- cart.py
+++ cart.py
@@ -8,25 +8,25 @@ class ShoppingCart:
                 {
                     'name': 'Unlimited 1GB',
                     'price': 24.90
-                },
+                }
             ],
             'ult_medium': [
                 {
                     'name': 'Unlimited 2GB',
                     'price': 29.90
-                },
+                }
             ],
             'ult_large': [
                 {
                     'name': 'Unlimited 5GB',
                     'price': 44.90
-                },
+                }
             ],
             '1gb': [
                 {
                     'name': '1GB Data-pack',
                     'price': 9.90
-                },
+                }
             ]
         }
         self.cart = {}
